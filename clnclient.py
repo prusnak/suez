c79c93fd431182176f85798fed19d6660c11d14e
Sebastian Andersson <sebastianandersson@zoho.eu>
2023-03-24 16:38:06 +0100

    Use setchannel CLN command

--- clnclient.py
+++ clnclient.py
@@ -146,7 +146,7 @@ class ClnClient:
             if c.chan_id is not None:
                 base_fee, fee_rate, _ = policy.calculate(c)
                 self._run(
-                    "setchannelfee",
+                    "setchannel",
                     c.chan_id,
                     str(base_fee),
                     str(int(fee_rate * 1000000)),
